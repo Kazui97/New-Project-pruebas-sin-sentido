b1dc3889146be9d80220d0bfbbf2e360d721ea3c
Kazui97 <camyescobar12@gmail.com>
2019-11-11 13:32:57 -0500

    Initial commit

---
+++ Assets/scrits/Enemy.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    // variables para gestionar la vision y velocidad 
+    public float vision;
+    public float speed;
+
+    //variables para guardar el jugador 
+    GameObject player;
+
+    Vector3 posicioninicial;
+
+    void Start()
+    {
+        //recuperamos al jugador gracias al tag
+        player = GameObject.FindGameObjectWithTag("Player");
+        //guardamos la posicion inicial 
+        posicioninicial = transform.position;
+    }
+
+   
+    void Update()
+    {
+        // por defecto nuestro objeto siempre sera la posicion incial 
+        Vector3 target = posicioninicial;
+
+        // pero si la distancia hasta el jugador es menor que el radio de vision de objetivo sera el 
+        float dist = Vector3.Distance(player.transform.position, transform.position);
+        if (dist < vision) target = player.transform.position;
+
+        // finalmente movemos el enemigo en direccion a su target
+        float fixedspeed = speed * Time.deltaTime;
+        transform.position = Vector3.MoveTowards(transform.position, target, fixedspeed);
+        
+        //podemos debugearlo con una linea
+        Debug.DrawLine(transform.position, target, Color.green);
+    }
+
+    //podemos dibujar el radio de vision sobre la escena 
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawWireSphere(transform.position, vision);
+    }
+}
---
+++ Assets/scrits/Movimiento.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movimiento : MonoBehaviour
+{
+    // variable para gestionar la velocidad 
+    public float speed;
+    // variable para establecer un punto de destino
+    Vector3 destino;
+    void Start()
+    {
+        //inicialmente el punto de destino de la posicion actual
+        destino = transform.position;
+    }
+
+   
+    void Update()
+    {
+        //detectamos cuando hacemos clic 
+        if (Input.GetMouseButtonDown(0))
+        {
+            //buscamos la posicion del clic respecto a la escena
+            destino = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            //establecemos la z en cero para que no modifique la profundidad  
+            destino.z = 0f;
+        }
+        //movemos el objeto hacia el punto de destino a una velocidad rectificada
+        transform.position = Vector3.MoveTowards(transform.position, destino, speed * Time.deltaTime);
+        //debugea una linea con el trayecto a recorer
+        Debug.DrawLine(transform.position, destino, Color.green);
+
+    }
+}
